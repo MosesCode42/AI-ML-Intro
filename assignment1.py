187a79bf019b9dc06316dbab4b70349f99d65b0e
Moses Omondi <130668089+MosesCodeX@users.noreply.github.com>
2023-05-21 11:34:47 +0300

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. In the below elements which of them are values or an expression? eg:- values can be
+# integer or string and expressions will be mathematical operators.
+# 
+
+# They are all values and expressions.
+# * is mathematical operator for multiplication
+# 'hello' is a string value which can also be written using double quotes "hello"
+# -87.8 is a floating point value or a decimal point number
+# - is a minus used for subtraction
+# / is division operator which will always return a float number
+# + is an addition operator for adding integers and concatination 
+# 6 is an integer number or a whole number
+# 
+
+# 2. What is the difference between string and variable?
+
+# A string is a value that represents a text using a single or double quotes while a variable is the name give to store any type of information or data type.
+
+# 3. Describe three different data types.
+
+# -> Text data type which is a String
+# -> Numeric data types which include integers (whole numbers), floating point numbers (decimal numbers) and comlex data types that consists of two values (real part and imaginary part) 
+# ->Boolean data type is data type that has two possible values True or False
+
+# 4. What is an expression made up of? What do all expressions do?
+
+# An expression is made up values, variables, operators and calls to function that needs to be evaluated.
+# Expressions produces results after being interpreted by the Python interpreter 
+
+# 5. This assignment statements, like spam = 10. What is the difference between an
+# expression and a statement?
+
+# An expression produces results after being interpreted while a statement is a line of code commanding a task for the result to be produced. 
+
+# 6. After running the following code, what does the variable bacon contain?
+# bacon = 22
+# bacon + 1
+
+# 23
+
+# 7. What should the values of the following two terms be?
+# 'spam' + 'spamspam'
+# 'spam' * 3
+
+# They all produce the same value 
+# spamspamspam
+
+# 8. Why is eggs a valid variable name while 100 is invalid?
+
+# It is the conventional way, you cannot use an integer literal in Python such as 40. Variable names must follow certain rules
+
+# 9. What three functions can be used to get the integer, floating-point number, or string
+# version of a value?
+
+# int()
+# float()
+# str()
+
+# 10. Why does this expression cause an error? How can you fix it?
+# 'I have eaten' + 99 + 'burritos.'
+# 
+
+# It is not possible to concatinate a string with an integer. You fix it by '99'
+# 
+# 'I have eaten'+'99'+'burriots'
+
+# In[ ]:
+
+
+
+
